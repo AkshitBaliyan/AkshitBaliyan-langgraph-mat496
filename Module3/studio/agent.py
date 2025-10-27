ef5530633b4d73c60d5985ae638425d762ce8c86
Akshit Baliyan <akshitbaliyan0248@gmail.com>
2025-10-27 20:12:05 +0530

    done video 5, updated agent.py as well accordinly

--- Module3/studio/agent.py
+++ Module3/studio/agent.py
@@ -23,7 +23,7 @@ def multiply(a: int, b: int) -> int:
     return a * b
 
 def divide(a: int, b: int) -> float:
-    """Adds a and b.
+    """Divide a by b.
 
     Args:
         a: first int
@@ -31,7 +31,16 @@ def divide(a: int, b: int) -> float:
     """
     return a / b
 
-tools = [add, multiply, divide]
+def subtract(a: int, b: int) -> int:
+    """Subtract b from a.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a - b
+
+tools = [add, multiply, divide, subtract]
 
 # Define LLM with bound tools
 llm = ChatOpenAI(model="gpt-4o")
